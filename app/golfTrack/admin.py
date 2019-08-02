942dc7b3728c94967b9d15d6f226f2cf8ac2fc94
chapman3 <chapman3@uoregon.edu>
2019-08-02 10:55:41 -0700

    added golfTrack models to admin

--- app/golfTrack/admin.py
+++ app/golfTrack/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Hole, Course, Scores, Round, Scorecard
 # Register your models here.
+admin.site.register(Hole)
+admin.site.register(Course)
+admin.site.register(Scores)
+admin.site.register(Round)
+admin.site.register(Scorecard)
