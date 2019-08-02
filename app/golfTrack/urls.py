abd746d7158b6f2907d173af98eb9e221c8b3d6f
chapman3 <chapman3@uoregon.edu>
2019-08-02 16:16:26 -0700

    setup super basic course views

--- app/golfTrack/urls.py
+++ app/golfTrack/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('courses/', views.course_list, name="course_list"),
+    path('<int:course_id>/', views.course_detail, name='course_detail'),
 ]
--- app/golfTrack/views.py
+++ app/golfTrack/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
 def index(request):
     return HttpResponse("This is the index")
+
+def course_list(request):
+    courses = Course.objects.all()
+    output = ', '.join([c.__str__() for c in courses])
+    return HttpResponse(output)
+
+def course_detail(request, course_id):
+    course = Course.objects.all()[course_id-1]
+    output = course.__str__()
+    return HttpResponse("You're looking at course %s." % output)
